722b23c7a76030be68d85519b8186e37d2fb0e0a
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2020-04-20 12:07:22 +0200

    Update config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-   debug: true,
-   lang: 'en',
-   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) Gecko/20100101 Firefox/70.0',
+   debug: true /* True means visible browser, false unvisible */,
+   lang: 'en' /* Log's language in console */,
+   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) Gecko/20100101 Firefox/70.0' /* Optional *,
    username: '<username>' /* username */,
    password: '<password>' /* password */,
    hashtags: ['cars', 'warsaw', 'szczecin'] /* hashtags */,
    likesPerHashtag: 5 /* likes for one hashtag */,
    breakBetweenLikes: 15000 /* breakdown between likes */,
    breakBetweenHashtags: 60000 /* breakdown between hashtags */,
-   mode: 'autolike',
+   mode: 'autolike' /* Currently only available autolike */,
 };
